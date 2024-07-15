d5aea413c66a9fe2e365ea00f4c31d864e1bba6c
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-15 13:23:28 -0700

    PagosDescuentos
    Cambios en las relaciones y nuevos modelos pago-descuentos

--- database/migrations/2024_07_08_165158_create_toma_table.php
+++ database/migrations/2024_07_08_165158_create_toma_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->unsignedBigInteger('id_codigo_toma');
 
             $table->string('clave_catastral');
-            $table->string('estatus');
+            $table->enum('estatus', ['domestica', 'comercial', 'industrial']);
             $table->string('calle');
             $table->string('entre_calle_1')->nullable();
             $table->string('entre_calle_2')->nullable();
@@ -31,10 +31,10 @@ return new class extends Migration
             $table->string('entre_calle_notificaciones_1')->nullable();
             $table->string('entre_calle_notificaciones_2')->nullable();
             $table->string('tipo_servicio');
-            $table->enum('tipo_toma', ['domestica', 'comercial', 'industrial']);
+            $table->enum('tipo_toma', ['pendiente confirmación inspección', 'pendiente de inspeccion', 'pendiente de instalacion', 'activa', 'baja definitiva', 'baja temporal', 'en proceso']);
             $table->unsignedBigInteger('c_agua')->nullable();
             $table->unsignedBigInteger('c_alc_san')->nullable();
-            $table->string('tipo_contratacion');
+            $table->enum('tipo_contratacion', ['normal', 'condicionado', 'desarrollador']);
             $table->softDeletes();
             $table->timestamps();
         });
