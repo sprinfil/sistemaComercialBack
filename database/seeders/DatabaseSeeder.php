2cc6912b4479b1ffb7120633e0d442eee30ac556
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-17 09:23:08 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
+        //
         \App\Models\User::factory()->create([
             'name' => 'admin',
             'email' => 'test@example.com',
