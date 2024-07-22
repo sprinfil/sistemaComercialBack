60ca86573be6ead94b8987a85322393d1ab48130
Jeremy Ojeda Ceseña <hola2001.com@hotmail.com>
2024-07-22 11:53:31 -0700

    rol

--- routes/api.php
+++ routes/api.php
@@ -286,6 +286,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
         Route::post("Rol/give_rol_permissions/{id}", "give_rol_permissions");
         Route::get("Rol/get_all_permissions_by_rol_id/{id}", "get_all_permissions_by_rol_id");
+        Route::get("Rol/get_all_permissions_by_user_id/{id}", "get_all_permissions_by_user_id");
 
         //log delete significa borrado logico
         Route::delete("Rol/log_delete/{id}", "destroy");
