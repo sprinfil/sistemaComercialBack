f2b07eb97e483e38dcb171c957d044738c810a6b
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-24 11:39:28 -0700

    Create ColoniaResource.php

---
+++ app/Http/Resources/ColoniaResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ColoniaResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Transform the resource into an array..
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "nombre" => $this->nombre
+        ];
+    }
+}
