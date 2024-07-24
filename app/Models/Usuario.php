5bf77a8e5b490f8233519169469ef93f0a616ff3
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-24 14:33:34 -0700

    fixdelfixdelfix

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Usuario extends Model
 {
     use HasFactory, SoftDeletes;
+    protected $table = "usuarios";
     protected $fillable=[
         'codigo_usuario',
         'nombre',
