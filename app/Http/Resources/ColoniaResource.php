9e3123d3b8e22ece590e0654d09c614b6bf5ce4f
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-24 11:40:50 -0700

    Update ColoniaResource.php

--- app/Http/Resources/ColoniaResource.php
+++ app/Http/Resources/ColoniaResource.php
@@ -9,7 +9,7 @@ class ColoniaResource extends JsonResource
 {
     public static $wrap = false;
     /**
-     * Transform the resource into an array.
+     * Transform the resource into an array..
      *
      * @return array<string, mixed>
      */
