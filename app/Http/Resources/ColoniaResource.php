3ee115e7660476d6e6503f26a92890ba3e83ed15
Miguel A. Murillo <mgamj13@gmail.com>
2024-07-24 11:39:06 -0700

    fix

--- app/Http/Resources/ColoniaResource.php
+++ app/Http/Resources/ColoniaResource.php
@@ -16,6 +16,7 @@ class ColoniaResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "nombre" => $this->nombre
         ];
     }
