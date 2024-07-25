20c7c14bb8e23ef0ad56506da78593c2c0ad10c1
Martin Meza <martinmzp19@gmail.com>
2024-07-25 14:00:40 -0700

    relacionPeriodo
    relacion de periodos

--- app/Models/Ruta.php
+++ app/Models/Ruta.php
@@ -4,15 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ruta extends Model
 {
     use HasFactory,SoftDeletes;
+    protected $table = "rutas";
+
 
     protected $fillable = [
         "nombre"
     ];
 
+    public function tieneLibros() : HasMany {
+        return $this->hasMany(Libro::class , "id_ruta");
+    }
+
+    public function tienePeriodos() : HasMany {
+        return $this->hasMany(Periodo::class , "id_ruta");
+    }
+    
     //pendiente relaciones de ruta
 }
