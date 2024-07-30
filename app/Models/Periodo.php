87405fdbf07927ded3bd761baf39f8fa5a67f7f1
Martin Meza <martinmzp19@gmail.com>
2024-07-30 09:22:34 -0700

    fixCargaTrabajo

--- app/Models/Periodo.php
+++ app/Models/Periodo.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Periodo extends Model
 {
@@ -20,5 +21,8 @@ class Periodo extends Model
 
     public function tieneRutas() : BelongsTo {
         return $this->belongsTo(Ruta::class , 'id_ruta');
+    }
+    public function cargaTrabajo() : HasMany {
+        return $this->hasMany(cargaTrabajo::class , "id_periodo");
     }
 }
