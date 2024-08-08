176f99407cae6c4b1d6c0206cabbe05c55d2fe25
Miguel A. Murillo <mgamj13@gmail.com>
2024-08-08 13:54:06 -0700

    jijijija

--- routes/api/caja/cargo.php
+++ routes/api/caja/cargo.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CargoController;
-use App\Http\Controllers\Api\cargoDirectoController;
+use App\Http\Controllers\Api\CargoDirectoController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', 'audit'])->group(function () {
@@ -14,7 +14,7 @@ Route::middleware(['api', 'audit'])->group(function () {
         Route::get("/cargos/porModelo/pendientes","cargosPorModeloPendientes");
     });
     // cargo directo
-    Route::controller(cargoDirectoController::class)->group(function() {
+    Route::controller(CargoDirectoController::class)->group(function() {
         Route::get("/cargoDirecto","index");
         Route::post("/cargoDirecto/store","store");
         Route::get("/cargoDirecto/show/{id}" , "show");
