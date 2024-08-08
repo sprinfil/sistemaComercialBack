837d9d465f87cf03141b3fc41cab25d0157ba035
Miguel A. Murillo <mgamj13@gmail.com>
2024-08-08 13:43:25 -0700

    AA
    AA

--- app/Http/Controllers/Api/TarifaController.php
+++ app/Http/Controllers/Api/TarifaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTarifaConceptoDetalleRequest;
-use App\Models\tarifa;
+use App\Models\Tarifa;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTarifaRequest;
 use App\Http\Requests\StoreTarifaServiciosDetalleRequest;
