f1a7e06bfedd7bc3499ea50e5d016930f800fd23
Miguel A. Murillo <mgamj13@gmail.com>
2024-08-09 10:27:37 -0700

    +corecciones

--- database/factories/corteCajaFactory.php
+++ database/factories/corteCajaFactory.php
@@ -7,9 +7,9 @@ use App\Models\Pago;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\corteCaja>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CorteCaja>
  */
-class corteCajaFactory extends Factory
+class CorteCajaFactory extends Factory
 {
     /**
      * Define the model's default state.
