103ff6ac53d2ab57e0841aed3d3eb8b634a3a0d0
JosueASapa <josueacostamolinasfd@gmail.com>
2024-08-20 09:49:24 -0700

    iniciarCajaAPI

--- app/Http/Requests/UpdateCajasRequest.php
+++ app/Http/Requests/UpdateCajasRequest.php
@@ -22,28 +22,29 @@ class UpdateCajasRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "id_operador"=>"required|integer|gt:0",
-            "id_caja"=>"required|integer|gt:0",
 
             "caja_data"=>"required|array",
-            "caja_data.*.id_operador"=>"required|integer|gt:0",
-            "caja_data.*.fecha_apertura"=>"required|date_format:Y-m-d H:i:s",
-            "caja_data.*.fecha_cierre"=>"required|date_format:Y-m-d H:i:s",
+            "caja_data.*.id_operador" => "required|integer|gt:0",    
+            "caja_data.*.id_caja_catalogo" => "required|integer|gt:0",
+          //  "caja_data.*.fondo_inicial" => "nullable|numeric|regex:/^\d+(\.\d{2})$/",//esto no debe estar lleno en el cierre
+            "caja_data.*.fondo_final" => "required|numeric|regex:/^\d+(\.\d{2})$/",
+           // "caja_data.*.fecha_apertura" => "nullable|date_format:Y-m-d H:i:s", //esto no debe estar lleno en el cierre
+            "caja_data.*.fecha_cierre" => "required|date_format:Y-m-d H:i:s", 
 
             "corte_data"=>"required|array",
-            "corte_data.*.id_caja"=>"required|integer|gt:0",
+            //"corte_data.*.id_caja"=>"required|integer|gt:0",
             "corte_data.*.id_operador"=>"required|integer|gt:0",
-            "corte_data.*.estatus"=>"required|string|in:aprobado,rechazado",
-            "corte_data.*.total_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
+            "corte_data.*.estatus"=>"required|string|in:aprobado,rechazado,pendiente",
+            //"corte_data.*.total_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/", //Este lo calcula el sistema
             "corte_data.*.total_real"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
-            "corte_data.*.total_efectivo_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
+            //"corte_data.*.total_efectivo_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",//Este lo calcula el sistema
             "corte_data.*.total_efectivo_real"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
-            "corte_data.*.total_tarjetas_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
+            //"corte_data.*.total_tarjetas_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",//Este lo calcula el sistema
             "corte_data.*.total_tarjetas_real"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
-            "corte_data.*.total_cheques_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
+            //"corte_data.*.total_cheques_registrado"=>"required|numeric|regex:/^\d+(\.\d{2})$/",//Este lo calcula el sistema
             "corte_data.*.total_cheques_real"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
-            "corte_data.*.discrepancia"=>"required|string|in:si,no",
-            "corte_data.*.discrepancia_monto"=>"required|numeric|regex:/^\d+(\.\d{2})$/",
+            //"corte_data.*.discrepancia"=>"required|string|in:si,no",//Este lo calcula el sistema
+            //"corte_data.*.discrepancia_monto"=>"required|numeric|regex:/^\d+(\.\d{2})$/",//Este lo calcula el sistema
             "corte_data.*.fecha_corte"=>"required|date_format:Y-m-d H:i:s",
         ];
     }
