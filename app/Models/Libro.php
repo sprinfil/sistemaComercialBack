e7418200fbadf0625670b7f79b852770fa2b4d8c
Alan Jacob <kou4_alan@hotmail.com>
2024-09-04 21:54:24 -0700

    asasa

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -35,7 +35,7 @@ class Libro extends Model
         'polygon' => Polygon::class,
     ];
  
-    public function ruta() : BelongsTo {
+    public function tieneRuta() : BelongsTo {
         return $this->belongsTo(Ruta::class , "id_ruta");
     }
     public function cargaTrabajo() : HasMany {
