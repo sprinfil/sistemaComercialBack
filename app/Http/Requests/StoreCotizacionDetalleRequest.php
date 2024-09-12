d74ce1d7c778154a83686ecf44c6e5f78805af5c
Alan Jacob <80935855+Mayonaisa@users.noreply.github.com>
2024-09-12 14:27:20 -0700

    cotizacion detalle 3.0

--- app/Http/Requests/StoreCotizacionDetalleRequest.php
+++ app/Http/Requests/StoreCotizacionDetalleRequest.php
@@ -22,9 +22,10 @@ class StoreCotizacionDetalleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "cotizacion_detalle"=>"required|array",
-            "cotizacion_detalle.*"=>"required|array",//:id_cotizacion,id_sector,nombre_concepto,monto
-            "cotizacion_detalle.*.id_cotizacion"=>"required|exists:cotizaciones,id",
+            "id_contrato"=>"required|exists:contratos,id",
+            "cotizacion_detalle"=>"sometimes|array",
+            "cotizacion_detalle.*"=>"sometimes|array",//:id_cotizacion,id_sector,nombre_concepto,monto
+            //"cotizacion_detalle.*.id_cotizacion"=>"required|exists:cotizaciones,id",
             "cotizacion_detalle.*.id_sector"=>"sometimes", #todo
             "cotizacion_detalle.*.id_concepto"=>"sometimes|integer",
             "cotizacion_detalle.*.monto"=>"sometimes|numeric",
