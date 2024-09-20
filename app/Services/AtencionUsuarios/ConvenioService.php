ec73324db6e3ad7653b32fb145456230b58eca0d
JosueASapa <josueacostamolinasfd@gmail.com>
2024-09-20 12:51:01 -0700

    MontoPendienteConsultarCargos

--- app/Services/AtencionUsuarios/ConvenioService.php
+++ app/Services/AtencionUsuarios/ConvenioService.php
@@ -56,16 +56,18 @@ class ConvenioService{
             ->where('id_modelo',$data['id_convenio_catalogo'])
             ->get();
             
+            $cargo_selecionado = Cargo::findOrFail($cargo['id_concepto']);
           
             if (count($temp) != 0) {
               $cargosAplicables[$nxt] = $cargo;
               $cargosAplicables[$nxt]['aplicable'] = "si";
               $cargosAplicables[$nxt]['rango_minimo'] = $temp[0]['rango_minimo'];
-              $cargosAplicables[$nxt]['rango_maximo'] = $temp[0]['rango_maximo'];             
+              $cargosAplicables[$nxt]['rango_maximo'] = $temp[0]['rango_maximo'];         
+              $cargosAplicables[$nxt]['monto_pendiente'] = $cargo_selecionado->montoPendiente();             
             }
             else{
               $cargosAplicables[$nxt] = $cargo;
-              $cargosAplicables[$nxt]['aplicable'] = "No";
+              $cargosAplicables[$nxt]['aplicable'] = "No";          
             }          
             $nxt++;
           }
