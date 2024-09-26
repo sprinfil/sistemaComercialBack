e9bb6107fa49cfb72a0e65d6b10c0d202781e4fb
JosueASapa <josueacostamolinasfd@gmail.com>
2024-09-26 14:39:05 -0700

    TipoTomaAplicableApi
    Tipos de tomas aplicables a ciertos procesos, Api´s generales y sus rutas

--- app/Http/Requests/StoreTipoTomaAplicableRequest.php
+++ app/Http/Requests/StoreTipoTomaAplicableRequest.php
@@ -11,7 +11,7 @@ class StoreTipoTomaAplicableRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class StoreTipoTomaAplicableRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "id_modelo" => "required|integer|gt:0",
+
+             "modelo_origen" => "required|string|
+             in:ajuste_catalogo,descuento_catalogo,convenio_catalogo",
+
+             "id_tipo_toma" => "required|integer|gt:0",
         ];
     }
 }
