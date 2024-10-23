fa4b19551e470d01fb4725f142a54aa2cbb8f2fe
Martin Meza <martinmzp19@gmail.com>
2024-10-23 10:36:08 -0700

    rutas de validacion
    rutas de validación para modificar consumo y lectura por periodo

--- routes/api/lectura/validacion.php
+++ routes/api/lectura/validacion.php
@@ -8,6 +8,8 @@ Route::middleware(['api', 'audit'])->group(function () {
     Route::controller(ValidacionController::class)->group(function(){
       Route::get("/validacion/periodo/{id}" , "consumosperiodo");
       Route::post("/validacion/consumo/registrarconsumo" , "registrarconsumo");
+      Route::post("/validacion/consumo/modificarconsumo" , "modificarconsumo");
+      Route::post("/validacion/consumo/modificarlectura{id_periodo}" , "modificarlectura");
       Route::post("/valiacion/consumo/promediar" , "promediar");
     });
 });
